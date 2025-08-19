46c3d80778a22063eebf7a63fcb2b84e85ffe0f0
abbas <akhtarabbas@navapbc.com>
2025-08-19 11:09:50 -0400

    added custom domain and certs/https

--- infra/project-config/networks.tf
+++ infra/project-config/networks.tf
@@ -4,17 +4,17 @@ locals {
       account_name = "nava-community-engagement-medicaid"
 
       domain_config = {
-        manage_dns = false
+        manage_dns = true
         # Placeholder value for the hosted zone
         # A hosted zone represents a domain and all of its subdomains. For example, a
         # hosted zone of foo.domain.com includes foo.domain.com, bar.foo.domain.com, etc.
-        hosted_zone = null
+        hosted_zone = "engage.navateam.com"
 
         certificate_configs = {
-          # Example certificate configuration for a certificate that is managed by the project
-          # "sub.domain.com" = {
-          #   source = "issued"
-          # }
+         # Example certificate configuration for a certificate that is managed by the project
+          "engage.navateam.com" = {
+            source = "issued"
+          }
 
           # Example certificate configuration for a certificate that is issued elsewhere and imported into the project
           # (currently not supported, will be supported via https://github.com/navapbc/template-infra/issues/559)
