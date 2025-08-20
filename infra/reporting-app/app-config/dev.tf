a12d0c19cef6d31b7fe4c157a14d45e887682af8
abbas <akhtarabbas@navapbc.com>
2025-08-20 13:23:54 -0400

    add database/app config

--- infra/reporting-app/app-config/dev.tf
+++ infra/reporting-app/app-config/dev.tf
@@ -5,8 +5,8 @@ module "dev_config" {
   default_region                  = module.project_config.default_region
   environment                     = "dev"
   network_name                    = "dev"
-  domain_name                     = "engage.navateam.com"
-  enable_https                    = true
+  domain_name                     = null
+  enable_https                    = false
   has_database                    = local.has_database
   has_incident_management_service = local.has_incident_management_service
   enable_notifications            = local.enable_notifications
