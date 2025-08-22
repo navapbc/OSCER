16cb0d931ca4c853ccef3206f2187fcbc965357c
Loren Yu <loren@navapbc.com>
2025-08-22 13:32:31 -0700

    Fleshed out review activity report detail view

--- reporting-app/app/controllers/tasks_controller.rb
+++ reporting-app/app/controllers/tasks_controller.rb
@@ -1,7 +1,16 @@
 class TasksController < Flex::TasksController
-  def show
-    super
-    @kase = ActivityReportCase.find(@task.case_id)
-    @application_form = ActivityReportApplicationForm.find(@kase.application_form_id)
+  before_action :set_case, only: %i[ show update ]
+  before_action :set_application_form, only: %i[ show update ]
+
+  private
+
+  def set_case
+    # TODO: Flex::TasksController should automatically set the case, but can't right now
+    # It should be able to after https://linear.app/nava-platform/issue/TSS-276/add-case-type-to-flex-tasks
+    @case = ActivityReportCase.find(@task.case_id)
+  end
+
+  def set_application_form
+    @application_form = ActivityReportApplicationForm.find(@case.application_form_id)
   end
 end
