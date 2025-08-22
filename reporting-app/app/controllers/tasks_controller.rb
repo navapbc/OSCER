ea6c4b0bb5fe62a10a31dc3a92a02be4b5980c18
Loren Yu <loren@navapbc.com>
2025-08-22 11:05:30 -0700

    Add stub review activity report task view

--- reporting-app/app/controllers/tasks_controller.rb
+++ reporting-app/app/controllers/tasks_controller.rb
@@ -1,2 +1,7 @@
 class TasksController < Flex::TasksController
+  def show
+    super
+    @kase = ActivityReportCase.find(@task.case_id)
+    @application_form = ActivityReportApplicationForm.find(@kase.application_form_id)
+  end
 end
