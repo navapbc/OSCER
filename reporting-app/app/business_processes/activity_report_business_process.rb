07f3c8f68320f5d7fe7f48af4993c8baa52d176e
Loren Yu <loren@navapbc.com>
2025-08-21 17:22:00 -0700

    Lint

--- reporting-app/app/business_processes/activity_report_business_process.rb
+++ reporting-app/app/business_processes/activity_report_business_process.rb
@@ -1,9 +1,9 @@
 class ActivityReportBusinessProcess < Flex::BusinessProcess
-  applicant_task('submit_report')
-  staff_task('review_report', ReviewActivityReportTask)
+  applicant_task("submit_report")
+  staff_task("review_report", ReviewActivityReportTask)
 
-  start_on_application_form_created('submit_report')
+  start_on_application_form_created("submit_report")
 
-  transition('submit_report', 'ActivityReportApplicationFormSubmitted', 'review_report')
-  transition('review_report', 'ActivityReportReviewed', 'end')
+  transition("submit_report", "ActivityReportApplicationFormSubmitted", "review_report")
+  transition("review_report", "ActivityReportReviewed", "end")
 end
--- reporting-app/app/models/review_activity_report_task.rb
+++ reporting-app/app/models/review_activity_report_task.rb
@@ -1,6 +1,6 @@
 class ReviewActivityReportTask < Flex::Task
   # Add custom attributes and behavior here
-  
+
   # Example:
   # attribute :custom_field, :string
   # validates :custom_field, presence: true
