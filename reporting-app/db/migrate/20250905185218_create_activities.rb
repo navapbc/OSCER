f0fc416106ce781c4a972cb071c096c72b58f3f7
Loren Yu <loren@navapbc.com>
2025-09-05 11:53:40 -0700

    Add activities table

---
+++ reporting-app/db/migrate/20250905185218_create_activities.rb
@@ -0,0 +1,12 @@
+class CreateActivities < ActiveRecord::Migration[7.2]
+  def change
+    create_table :activities, id: :uuid do |t|
+      t.references :activity_report_application_form, null: false, foreign_key: true, type: :uuid
+      t.date :month
+      t.decimal :hours
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
