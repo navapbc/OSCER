35cd46a31ef5004aa26c3bdd7bf8219b3d1789b3
Loren Yu <loren@navapbc.com>
2025-09-05 17:00:17 -0700

    Add supporting_documents attribute

--- reporting-app/app/models/activity.rb
+++ reporting-app/app/models/activity.rb
@@ -1,6 +1,10 @@
 class Activity < ApplicationRecord
   include Flex::Attributes
+
   belongs_to :activity_report_application_form
+  has_many_attached :supporting_documents
+
+  default_scope { includes(:activity_report_application_form) }
 
   flex_attribute :month, :date
   flex_attribute :hours, :decimal
