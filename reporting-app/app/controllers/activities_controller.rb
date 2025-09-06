73d11ac2076defad7de43c0588a175c77b06f87e
Loren Yu <loren@navapbc.com>
2025-09-05 17:31:20 -0700

    Add edit activity view

--- reporting-app/app/controllers/activities_controller.rb
+++ reporting-app/app/controllers/activities_controller.rb
@@ -40,7 +40,7 @@ class ActivitiesController < ApplicationController
   def update
     respond_to do |format|
       if @activity.update(activity_params)
-        format.html { redirect_to @activity, notice: "Activity was successfully updated." }
+        format.html { redirect_to @activity.activity_report_application_form, notice: "Activity was successfully updated." }
         format.json { render :show, status: :ok, location: @activity }
       else
         format.html { render :edit, status: :unprocessable_entity }
