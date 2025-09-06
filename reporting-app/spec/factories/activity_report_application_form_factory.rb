2866b204b35eeeec3ce958a7e217e3a2127036c3
Loren Yu <loren@navapbc.com>
2025-09-06 00:08:33 -0700

    Update activity report factory

--- reporting-app/spec/factories/activity_report_application_form_factory.rb
+++ reporting-app/spec/factories/activity_report_application_form_factory.rb
@@ -3,5 +3,12 @@ FactoryBot.define do
     employer_name { Faker::Company.name }
     minutes { 15*rand(1..500) }
     reporting_period { Date.new(rand(2000..Date.today.year), rand(1..12), 1) }
+
+    trait :with_activities do
+      after(:create) do |activity_report_application_form|
+        activities = create_list(:activity, 3, activity_report_application_form_id: activity_report_application_form.id)
+        activity_report_application_form.activities = activities
+      end
+    end
   end
 end
