7572c8b934ac6cab6eb9865375e2628c3011b02c
Loren Yu <loren@navapbc.com>
2025-09-06 01:10:27 -0700

    Fix tests

--- reporting-app/spec/factories/activity_report_application_form_factory.rb
+++ reporting-app/spec/factories/activity_report_application_form_factory.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     employer_name { Faker::Company.name }
     minutes { 15*rand(1..500) }
     reporting_period { Date.new(rand(2000..Date.today.year), rand(1..12), 1) }
+    activities { [] }
 
     trait :with_activities do
       after(:create) do |activity_report_application_form|
