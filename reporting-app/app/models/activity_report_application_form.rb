4994df017173328891b1be0ab367bcab0b8d623d
Loren Yu <loren@navapbc.com>
2025-09-06 01:01:32 -0700

    Remove code for minutes

--- reporting-app/app/models/activity_report_application_form.rb
+++ reporting-app/app/models/activity_report_application_form.rb
@@ -17,23 +17,4 @@ class ActivityReportApplicationForm < Flex::ApplicationForm
   default_scope { includes(:activities) }
 
   accepts_nested_attributes_for :activities, allow_destroy: true
-
-  # Validation for minimum 15 minutes
-  validates :minutes, on: :submit, numericality: {
-    greater_than_or_equal_to: 15,
-    message: "must be at least 15 minutes"
-  }
-
-  validate :minutes_must_be_quarter_hour_increments
-
-  private
-
-  def minutes_must_be_quarter_hour_increments
-    return unless minutes.present?
-
-    # Check if minutes is a multiple of 15 (quarter hour)
-    unless (minutes % 15).zero?
-      errors.add(:minutes, "must be in quarter hour increments (15, 30, 45, 60, etc.)")
-    end
-  end
 end
