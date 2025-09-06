675c4114f38c9a1cebc3bc6187aa70c2ae2297f0
Loren Yu <loren@navapbc.com>
2025-09-06 00:07:37 -0700

    Update activities factory association

--- reporting-app/spec/factories/activities_factory.rb
+++ reporting-app/spec/factories/activities_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :activity do
-    activity_report_application_form { create(:activity_report_application_form) }
+    association :activity_report_application_form
     month { Date.today.prev_month.beginning_of_month }
     hours { Faker::Number.between(from: 1.0, to: 100.0) }
     name { Faker::Company.name }
