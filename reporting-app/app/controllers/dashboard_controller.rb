afadbde31c1fd4908ef977e85285c59106d80e7f
Bao Nguyen <baonguyen@navapbc.com>
2025-09-09 14:08:03 -0600

    Fix spacing

--- reporting-app/app/controllers/dashboard_controller.rb
+++ reporting-app/app/controllers/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class DashboardController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    # GET /dashboard or /dashboard.json
-    def index
-        @activity_report_application_forms = policy_scope(ActivityReportApplicationForm).order(created_at: :desc)
-        @in_progress_activity_reports = @activity_report_application_forms.in_progress
-    end
+  # GET /dashboard or /dashboard.json
+  def index
+    @activity_report_application_forms = policy_scope(ActivityReportApplicationForm).order(created_at: :desc)
+    @in_progress_activity_reports = @activity_report_application_forms.in_progress
+  end
 end
