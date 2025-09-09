83e12d8123b75708370e1aa7b7e3f06b7e53b2f4
Loren Yu <loren@navapbc.com>
2025-09-09 09:54:54 -0700

    Fix seeds

--- reporting-app/db/seeds/development.rb
+++ reporting-app/db/seeds/development.rb
@@ -1,7 +1,5 @@
 5.times do
   app_form = ActivityReportApplicationForm.create!(
-    employer_name: Faker::Company.name,
-    minutes: Faker::Number.between(from: 30, to: 1200).div(15) * 15, # Ensure it's a multiple of 15
     reporting_period: Date.today.prev_month.beginning_of_month
   )
   app_form.save!
