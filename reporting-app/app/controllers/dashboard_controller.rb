a1807ab753a7c898383f95c4a4c7d2f7293995b0
Bao Nguyen <baonguyen@navapbc.com>
2025-09-09 13:40:52 -0600

    lint fix

--- reporting-app/app/controllers/dashboard_controller.rb
+++ reporting-app/app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
     before_action :authenticate_user!
 
-      # GET /dashboard or /dashboard.json
+    # GET /dashboard or /dashboard.json
     def index
         @activity_report_application_forms = policy_scope(ActivityReportApplicationForm).order(created_at: :desc)
         @in_progress_activity_reports = @activity_report_application_forms.in_progress
