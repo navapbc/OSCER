95b03d026cc5b792f59dec9992e126321ce0f249
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-09-11 14:32:06 -0500

    Fix linter error

--- reporting-app/spec/models/activity_report_application_form_spec.rb
+++ reporting-app/spec/models/activity_report_application_form_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ActivityReportApplicationForm, type: :model do
   let (:form) { create(:activity_report_application_form) }
-  
+
   describe '#sum_of_activity_hours' do
     it 'returns the sum of all activity hours' do
       create(:activity, hours: 2.5, activity_report_application_form_id: form.id)
       create(:activity, hours: 3.0, activity_report_application_form_id: form.id)
       create(:activity, hours: 1.5, activity_report_application_form_id: form.id)
-      
+
       form.reload
       expect(form.sum_of_activity_hours).to eq(7.0)
     end
@@ -41,7 +41,7 @@ RSpec.describe ActivityReportApplicationForm, type: :model do
       create(:activity, hours: 2.0, month: Date.new(2025, 1, 1), activity_report_application_form_id: form.id)
 
       form.reload
-      
+
       # Total hours = 5.0, number of unique months = 1
       expect(form.average_of_activity_hours_per_month).to eq(5.0)
     end
