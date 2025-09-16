e3cced8ff0d5a5d7fcee889435d8482b829d1180
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-09-16 11:27:35 -0500

    Removed unnecessary code

--- reporting-app/spec/factories/activity_report_case_factory.rb
+++ reporting-app/spec/factories/activity_report_case_factory.rb
@@ -1,10 +1,5 @@
 FactoryBot.define do
   factory :activity_report_case do
-    after(:create) do |case_instance|
-      application_form = create(:activity_report_application_form, :with_activities)
-      case_instance.update!(application_form_id: application_form.id)
-    end
-
     initialize_with {
       application_form = create(:activity_report_application_form, :with_activities)
       application_form.submit_application
