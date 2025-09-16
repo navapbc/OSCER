6ee0b0f832358274f8d73f0b5d5c551d9ba29d99
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-09-16 10:47:04 -0500

    Change initialization to use initialize_with

--- reporting-app/spec/factories/activity_report_case_factory.rb
+++ reporting-app/spec/factories/activity_report_case_factory.rb
@@ -1,15 +1,16 @@
 FactoryBot.define do
   factory :activity_report_case do
     after(:create) do |case_instance|
-      application_form = create(:activity_report_application_form)
+      application_form = create(:activity_report_application_form, :with_activities)
       case_instance.update!(application_form_id: application_form.id)
     end
 
-    trait :with_activities do
-      after(:create) do |case_instance|
-        application_form = create(:activity_report_application_form, :with_activities)
-        case_instance.update!(application_form_id: application_form.id)
-      end
-    end
+    initialize_with {
+      application_form = create(:activity_report_application_form, :with_activities)
+      application_form.submit_application
+      application_form.reload
+
+      ActivityReportCase.find_or_create_by!(application_form_id: application_form.id)
+    }
   end
 end
