5e4084005b02723f381629e5577c9704277bb93c
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-09-17 13:54:25 -0500

    Swap to use array for consistency

--- reporting-app/spec/requests/activity_report_cases_spec.rb
+++ reporting-app/spec/requests/activity_report_cases_spec.rb
@@ -16,24 +16,24 @@ RSpec.describe "/staff/activity_report_cases", type: :request do
 
   describe "case sub-navigation pages" do
     [
-      { action: "show", path_method: :activity_report_case_path, label: "Details" },
-      { action: "tasks", path_method: :tasks_activity_report_case_path, label: "Tasks" },
-      { action: "documents", path_method: :documents_activity_report_case_path, label: "Documents" },
-      { action: "notes", path_method: :notes_activity_report_case_path, label: "Notes" }
-    ].each do |test_case|
+      [ "show", :activity_report_case_path, "Details" ],
+      [ "tasks", :tasks_activity_report_case_path, "Tasks" ],
+      [ "documents", :documents_activity_report_case_path, "Documents" ],
+      [ "notes", :notes_activity_report_case_path, "Notes" ]
+    ].each do |action, path_method, label|
       it "returns a success response" do
-        get send(test_case[:path_method], activity_report_case)
+        get send(path_method, activity_report_case)
         expect(response).to be_successful
       end
 
-      it "displays case #{test_case[:action]} page header" do
-        get send(test_case[:path_method], activity_report_case)
-        assert_select "#case-details-heading", text: test_case[:label], count: 1
+      it "displays case #{action} page header" do
+        get send(path_method, activity_report_case)
+        assert_select "#case-details-heading", text: label, count: 1
       end
 
-      it "sets the active sidenav to #{test_case[:action]}" do
-        get send(test_case[:path_method], activity_report_case)
-        assert_select ".usa-sidenav>.usa-sidenav__item>.usa-current", text: test_case[:label], count: 1
+      it "sets the active sidenav to #{action}" do
+        get send(path_method, activity_report_case)
+        assert_select ".usa-sidenav>.usa-sidenav__item>.usa-current", text: label, count: 1
       end
     end
   end
