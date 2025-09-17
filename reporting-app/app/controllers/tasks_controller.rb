67ffb0bccd6f44d5460e60d08376825bd4570ef4
Bao Nguyen <baonguyen@navapbc.com>
2025-09-17 11:05:23 -0600

    Cleanup task controller - set_case in sdk

--- reporting-app/app/controllers/tasks_controller.rb
+++ reporting-app/app/controllers/tasks_controller.rb
@@ -4,12 +4,6 @@ class TasksController < Flex::TasksController
 
   private
 
-  def set_case
-    # all tasks need the following association
-    # belongs_to :case, class_name: "ExampleCase"
-    @case = @task.case
-  end
-
   def set_application_form
     # all cases need the following association
     # has_one :application_form, foreign_key: :id, primary_key: :application_form_id, class_name: "ExampleApplicationForm"
