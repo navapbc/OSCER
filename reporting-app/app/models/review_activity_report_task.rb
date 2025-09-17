a07775e24c27c770b01ba2a019bf76d75f313438
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-09-17 10:33:49 -0500

    Update task statuses

--- reporting-app/app/models/review_activity_report_task.rb
+++ reporting-app/app/models/review_activity_report_task.rb
@@ -1,14 +1,17 @@
 class ReviewActivityReportTask < Flex::Task
   # Extend the status enum with new values
-  enum :status, { pending: 0, completed: 1, approved: 2, denied: 3 }
+  enum :status, { pending: 0, completed: 1, approved: 2, denied: 3 }, prefix: :mark
+  public attr_writer :status
 
-  def approve
-    self[:status] = :approved
-    save!
-  end
+  after_save :publish_completed_event, if: :saved_change_to_status?, on: :update
 
-  def deny
-    self[:status] = :denied
-    save!
+  private
+  def publish_completed_event
+    case status
+    when "approved", "denied"
+      Flex::EventManager.publish("#{self.class.name}#{status}", { task_id: id, case_id: case_id })
+    when "pending", "completed"
+      send("mark_#{status}") # the parent class handles these
+    end
   end
 end
