c46e464ed66cf3fe1a3e8858195f4abdbcea37ad
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-09-18 13:00:29 -0500

    Cleanup controller

--- reporting-app/app/controllers/review_activity_report_tasks_controller.rb
+++ reporting-app/app/controllers/review_activity_report_tasks_controller.rb
@@ -2,15 +2,33 @@ class ReviewActivityReportTasksController < TasksController
   def update
     @task.completed!
 
-    if params[:commit] == "approve"
+    if is_approving
       @task.approved!
-    elsif params[:commit] == "deny"
+    elsif is_denying
       @task.denied!
     end
 
     respond_to do |format|
-      format.html { redirect_to task_path(@task), notice: "Task completed." }
+      format.html { redirect_to task_path(@task), notice: task_complete_notice_text }
       format.json { render :show, status: :ok, location: task_path(@task) }
+    end
+  end
+
+  private
+
+  def is_approving
+    params[:commit] == t("tasks.details.review_activity_report_task.approve_button")
+  end
+
+  def is_denying
+    params[:commit] == t("tasks.details.review_activity_report_task.deny_button")
+  end
+
+  def task_complete_notice_text
+    if @task.approved?
+      t("tasks.details.review_activity_report_task.approved_message")
+    elsif @task.denied?
+      t("tasks.details.review_activity_report_task.denied_message")
     end
   end
 end
