50c2772f309fab760e34c08152398d4dd9f5f516
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-09-19 08:38:02 -0500

    Add method to handle case status

--- reporting-app/app/models/activity_report_case.rb
+++ reporting-app/app/models/activity_report_case.rb
@@ -1,2 +1,11 @@
 class ActivityReportCase < Flex::Case
+  store_accessor :facts, :activity_report_approval_status
+  def handle_review_task_completed(status) 
+    raise "Invalid status" unless ["approved", "denied"].include?(status)
+
+    self.activity_report_approval_status = status
+    save!
+
+    Flex::EventManager.publish("ActivityReportStatusUpdated", { case_id: id })
+  end
 end
