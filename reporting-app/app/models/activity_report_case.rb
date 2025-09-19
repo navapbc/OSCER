ecf10830a59908bb8d04979da6add8df199f0fc5
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-09-19 10:49:04 -0500

    Rearrange store_accessor

--- reporting-app/app/models/activity_report_case.rb
+++ reporting-app/app/models/activity_report_case.rb
@@ -1,8 +1,9 @@
 class ActivityReportCase < Flex::Case
   has_one :application_form, foreign_key: :id, primary_key: :application_form_id, class_name: "ActivityReportApplicationForm"
+  store_accessor :facts, :activity_report_approval_status
 
   default_scope { includes(:application_form) }
-  store_accessor :facts, :activity_report_approval_status
+  
   def handle_review_task_completed(status) 
     raise "Invalid status" unless ["approved", "denied"].include?(status)
 
