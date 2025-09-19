9dba2fe1a6cb475bc0e3d00f707b0e8a0836d5da
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-09-19 15:28:59 -0500

    Get cases for app forms

--- reporting-app/app/controllers/dashboard_controller.rb
+++ reporting-app/app/controllers/dashboard_controller.rb
@@ -4,6 +4,7 @@ class DashboardController < ApplicationController
   # GET /dashboard or /dashboard.json
   def index
     @activity_report_application_forms = policy_scope(ActivityReportApplicationForm).order(created_at: :desc)
+    @activity_report_cases = ActivityReportCase.where(application_form_id: @activity_report_application_forms.pluck(:id)).index_by(&:application_form_id)
     @exemption_application_forms = policy_scope(ExemptionApplicationForm).order(created_at: :desc)
     @in_progress_activity_reports = @activity_report_application_forms.in_progress
     @has_current_exemption = false # TODO: Exemptions will need to be retrieved from another source in the future
