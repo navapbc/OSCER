c86c4036c0c18fd164e73d1542b720710e3730c9
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-09-22 14:52:31 -0500

    Added updated_at timestamp for approval status

--- reporting-app/app/models/activity_report_case.rb
+++ reporting-app/app/models/activity_report_case.rb
@@ -1,10 +1,11 @@
 class ActivityReportCase < Flex::Case
-  store_accessor :facts, :activity_report_approval_status
+  store_accessor :facts, :activity_report_approval_status, :activity_report_approval_status_updated_at
 
   def handle_review_task_completed(status)
     raise "Invalid status" unless [ "approved", "denied" ].include?(status)
 
     self.activity_report_approval_status = status
+    self.activity_report_approval_status_updated_at = Time.current
     save!
 
     Flex::EventManager.publish("ActivityReportStatusUpdated", { case_id: id })
