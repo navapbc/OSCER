2477824c592496fa58137e0f8375a763a0039cb7
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-09-26 10:52:46 -0500

    Don't create activity report on certification create

--- reporting-app/app/controllers/certifications_controller.rb
+++ reporting-app/app/controllers/certifications_controller.rb
@@ -25,18 +25,8 @@ class CertificationsController < StaffController
     authorize @certification
 
     if @certification.save
-      # TODO: this logic could/should be moved to an business process/event
-      # processing step
-      is_exempt = false
-      if not is_exempt
-        # TODO: for demo purposes, create an activity report associated with the
-        # current user for this Certification
-        bene_user = current_user
-        activity_report = ActivityReportApplicationForm.create!(user_id: bene_user.id, certification: @certification)
-        # due to strict loading
-        @certification.activity_report_application_forms = [ activity_report ]
-      end
-
+      # due to strict loading
+      @certification.activity_report_application_forms = [ ]
       render :show, status: :created, location: @certification
     else
       render json: @certification.errors, status: :unprocessable_entity
