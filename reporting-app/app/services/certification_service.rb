efcfa43f185978fc426c2cc5f5192d12d8663990
Tanner Doshier <git@doshitan.com>
2025-09-29 13:33:23 -0400

    Filter dashboard to things associated with certs for current user

--- reporting-app/app/services/certification_service.rb
+++ reporting-app/app/services/certification_service.rb
@@ -11,13 +11,8 @@ class CertificationService
     # TODO: this logic could/should be moved to an business process/event
     # processing step
     is_exempt = false
-    if not is_exempt
-        # TODO: for demo purposes, create an activity report associated with the
-        # current user for this Certification
-        b_user = bene_user(certification) || current_user
-        if b_user
-            activity_report = ActivityReportApplicationForm.create!(user_id: b_user.id, certification: certification)
-        end
+    if is_exempt
+      # TODO: do something if automatically exempt
     end
 
     true
