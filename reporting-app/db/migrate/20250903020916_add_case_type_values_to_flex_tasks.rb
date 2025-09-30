5326022a176775f9b74a61873c75f44d126bdaa3
Bao Nguyen <baonguyen@navapbc.com>
2025-09-30 15:09:23 -0600

    Causes error due to table rename

--- reporting-app/db/migrate/20250903020916_add_case_type_values_to_flex_tasks.rb
+++ reporting-app/db/migrate/20250903020916_add_case_type_values_to_flex_tasks.rb
@@ -1,5 +1,5 @@
 class AddCaseTypeValuesToFlexTasks < ActiveRecord::Migration[7.2]
   def change
-    Strata::Task.where(case_type: nil).update_all(case_type: "ActivityReportCase")
+    # Strata::Task.where(case_type: nil).update_all(case_type: "ActivityReportCase")
   end
 end
