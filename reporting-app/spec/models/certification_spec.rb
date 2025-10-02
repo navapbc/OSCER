238eb44999404928df53ab026b93f3592001f50b
Bao Nguyen <baonguyen@navapbc.com>
2025-10-02 07:08:04 -0600

    Rename certification references #3

--- reporting-app/spec/models/certification_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Certification, type: :model do
-  it "has ActivityReportApplicationForm" do
-    certification = create(:certification, :with_activity_report_application_form)
-    expect(certification.activity_report_application_forms.count).to eq(1)
-  end
-end
