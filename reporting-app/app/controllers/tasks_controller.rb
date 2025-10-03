67cfcf317ba680503551c194042c3fa1e232c390
Bao Nguyen <baonguyen@navapbc.com>
2025-10-03 13:42:58 -0600

    Revise task controller with certification case

--- reporting-app/app/controllers/tasks_controller.rb
+++ reporting-app/app/controllers/tasks_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class TasksController < Strata::TasksController
+  before_action :set_certification, only: %i[ show update ]
+
+  private
+
+  def set_certification
+    @certification = Certification.find(@case.certification_id)
+  end
+
+  def set_application_form
+    # CertificationCase can be tied to different application forms
+    @application_form = ActivityReportApplicationForm.find_by(certification_id: @case.certification_id) ||
+                        ExemptionApplicationForm.find_by(certification_id: @case.certification_id)
+  end
 end
