a6fe00feb8d599fe5aae84c59298fa63bb2b0724
Loren Yu <loren@navapbc.com>
2025-10-03 13:14:49 -0700

    Customize case row

---
+++ reporting-app/app/components/certification_cases/case_row_component.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class CertificationCases::CaseRowComponent < ViewComponent::Base
+  class Case
+    attr_accessor :name, :case_number, :assignee, :step, :tasks, :created_at, :path
+
+    def initialize(certification_case:, certification:, path:)
+      # TODO: Once we're collecting the name, use the actual name rather than the email
+      @name = certification.beneficiary_account_email
+      @case_number = certification.case_number
+      @assignee = nil
+      @step = certification_case.business_process_instance.current_step
+      @tasks = certification_case.tasks
+      @created_at = certification_case.created_at
+      @path = path
+    end
+  end
+
+  def initialize(kase:, path_func:)
+    @case = kase
+    @path_func = path_func
+  end
+
+  def self.headers
+    [
+      t(".name"),
+      t(".case_number"),
+      t(".assignee"),
+      t(".step"),
+      t(".due_on"),
+      t(".created_at")
+    ]
+  end
+
+  private
+
+  def due_on(kase)
+    pending_tasks_with_due_date = kase.tasks.select { |task| task.pending? && task.due_on.present? }
+    pending_tasks_with_due_date.map(&:due_on).min&.strftime('%m/%d/%Y')
+  end
+end
--- reporting-app/app/controllers/certification_cases_controller.rb
+++ reporting-app/app/controllers/certification_cases_controller.rb
@@ -4,7 +4,17 @@ class CertificationCasesController < StaffController
   before_action :set_certification_elements, only: %i[ show tasks documents notes ]
 
   def index
-    @cases = CertificationCase.all
+    cases = CertificationCase.all
+    certification_ids = cases.map(&:certification_id)
+    certifications_by_id = Certification.where(id: certification_ids).index_by(&:id)
+    @cases = cases.map do |kase|
+      certification = certifications_by_id[kase.certification_id]
+      CertificationCases::CaseRowComponent::Case.new(
+        certification_case: kase,
+        certification: certification,
+        path: certification_case_path(kase)
+      )
+    end
   end
 
   def closed
