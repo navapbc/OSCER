9c7bc71ff34e2ba30707ac1c2e11077778f8cbb3
Loren Yu <yuloren@gmail.com>
2025-10-06 15:35:47 -0700

    Show email and case number on cases index page (#65)
    Adust cases index page
    - Add "name" column, using email for now to populate
    - Render certification.case_number rather than case.id
    
    ## Context for reviewers
    
    To help with demo usability and e2e testing, we want to show information
    from the original certification request on the certification cases page
    to better help identity the case to the request that was just made

---
+++ reporting-app/app/components/certification_cases/case_row_component.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class CertificationCases::CaseRowComponent < Strata::Cases::CaseRowComponent
+  def self.columns
+    [ :name ] + super
+  end
+
+  protected
+
+  def name
+    # TODO: Once we're collecting the name, use the actual name rather than the email
+    @case.certification.beneficiary_account_email
+  end
+
+  # Override default behavior to show the case number from the
+  # certification request rather than the case.id UUID
+  def case_no
+    link_to @case.certification.case_number, certification_case_path(@case)
+  end
+end
