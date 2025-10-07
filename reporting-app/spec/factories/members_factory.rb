f3614a667a7553a1ef57b9e077eb3d4a4b8ffbc6
Loren Yu <loren@navapbc.com>
2025-10-07 12:57:23 -0700

    Fix factory

--- reporting-app/spec/factories/members_factory.rb
+++ reporting-app/spec/factories/members_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :member do
     member_id { SecureRandom.alphanumeric(10) }
-    name { association(:name, :base) }
+    name { build(:name, :base) }
     email { Faker::Internet.email }
   end
 end
