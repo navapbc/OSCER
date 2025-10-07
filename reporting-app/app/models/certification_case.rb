06bd602ef8737ce421fe435f539da4e895b5fc6a
Loren Yu <loren@navapbc.com>
2025-10-07 15:47:47 -0700

    Don't override inherited attributes

--- reporting-app/app/models/certification_case.rb
+++ reporting-app/app/models/certification_case.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class CertificationCase < Strata::Case
-  attribute :certification_id, :uuid
-  attribute :status, :integer
-  attribute :business_process_current_step, :string
-  attribute :facts, :jsonb
+  # The following attributes are inherited from Strata::Case
+  # attribute :certification_id, :uuid
+  # attribute :status, :integer
+  # enum :status, open: 0, closed: 1
+  # attribute :business_process_current_step, :string
+  # attribute :facts, :jsonb
 
   # Don't add an ActiveRecord association since Certification
   # is a separate aggregate root and we don't want to add
