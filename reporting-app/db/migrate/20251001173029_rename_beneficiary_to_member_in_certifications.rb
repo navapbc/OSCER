ce29ea013b07a5cff2274db2d72eefa393d38be5
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-10-07 09:30:05 -0500

    Add frozen string literal comment

--- reporting-app/db/migrate/20251001173029_rename_beneficiary_to_member_in_certifications.rb
+++ reporting-app/db/migrate/20251001173029_rename_beneficiary_to_member_in_certifications.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameBeneficiaryToMemberInCertifications < ActiveRecord::Migration[7.2]
   def change
     rename_column :certifications, :beneficiary_id, :member_id
