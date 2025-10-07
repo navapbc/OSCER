35a4b4a24fa718e824dd8b4d72c9f0a31fac9906
Bao Nguyen <baonguyen@navapbc.com>
2025-10-07 15:06:41 -0600

    Add certification business process

--- reporting-app/app/models/certification_case.rb
+++ reporting-app/app/models/certification_case.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class CertificationCase < Strata::Case
-  attribute :certification_id, :uuid
-  attribute :status, :integer
-  attribute :business_process_current_step, :string
-  attribute :facts, :jsonb
-
   # Don't add an ActiveRecord association since Certification
   # is a separate aggregate root and we don't want to add
   # dependencies between the aggregates at the database layer
--- reporting-app/db/migrate/20251007205341_add_case_id_to_application_forms.rb
+++ reporting-app/db/migrate/20251007205341_add_case_id_to_application_forms.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddCaseIdToApplicationForms < ActiveRecord::Migration[7.2]
   def change
     add_reference :activity_report_application_forms, :certification_case, foreign_key: true, type: :uuid
