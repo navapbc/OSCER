c01b7b8eb4198c3ff9180c96980eefa4abd240de
Loren Yu <loren@navapbc.com>
2025-10-07 14:31:14 -0700

    Move search box to search page

--- reporting-app/app/controllers/members_controller.rb
+++ reporting-app/app/controllers/members_controller.rb
@@ -1,8 +1,16 @@
 class MembersController < StaffController
   def index
+    redirect_to search_members_path
   end
 
   def show
-    @member = Member.find(params[:id])
+    @certification = Certification.find_by_member_id(params[:id])
+  end
+
+  def search
+    @members = []
+    Certification.find_by_member_email(params[:email]).each do |certification|
+      @members << Member.new(member_id: certification.member_id, email: certification.member_email)
+    end
   end
 end
