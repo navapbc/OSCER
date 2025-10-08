791a5c0e6db8af3563f7ee89fd0ec3fe77255f7f
Bao Nguyen <baonguyen@navapbc.com>
2025-10-08 10:52:03 -0600

    Add spec for certification callback

--- reporting-app/spec/models/certification_spec.rb
+++ reporting-app/spec/models/certification_spec.rb
@@ -3,5 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Certification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'after_create_commit callback' do
+    it 'publishes CertificationCreated event with certification_id' do
+      allow(Strata::EventManager).to receive(:publish)
+      certification = build(:certification)
+
+      certification.save!
+      expect(Strata::EventManager).to have_received(:publish).with(
+        'CertificationCreated',
+        { certification_id: certification.id }
+      )
+    end
+  end
 end
