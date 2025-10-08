9173477eb0fdbce45404377fc0b97115d7edb5b9
Bao Nguyen <baonguyen@navapbc.com>
2025-10-08 05:33:50 -0600

    Get tests passing WIP

--- reporting-app/app/controllers/exemption_application_forms_controller.rb
+++ reporting-app/app/controllers/exemption_application_forms_controller.rb
@@ -11,7 +11,8 @@ class ExemptionApplicationFormsController < ApplicationController
   # GET /exemption_application_forms/new
   def new
     @exemption_application_form = authorize ExemptionApplicationForm.new(
-      certification_id: params[:certification_id]
+      certification_id: params[:certification_id],
+      certification_case_id: params[:certification_case_id]
     )
   end
 
@@ -108,7 +109,8 @@ class ExemptionApplicationFormsController < ApplicationController
     def exemption_application_form_params
       params.require(:exemption_application_form).permit(
         :exemption_type,
-        :certification_id
+        :certification_id,
+        :certification_case_id
         )
     end
 
