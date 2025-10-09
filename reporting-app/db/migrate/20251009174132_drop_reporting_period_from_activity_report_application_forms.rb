4420d1e030e703242cb7b27775b06ed5369a60cb
Mike Giver <mikegiver@navapbc.com>
2025-10-09 13:43:29 -0400

    Drop reporting_period from activity_report_application_forms

---
+++ reporting-app/db/migrate/20251009174132_drop_reporting_period_from_activity_report_application_forms.rb
@@ -0,0 +1,5 @@
+class DropReportingPeriodFromActivityReportApplicationForms < ActiveRecord::Migration[7.2]
+  def change
+    remove_column :activity_report_application_forms, :reporting_period, :date
+  end
+end
--- reporting-app/db/schema.rb
+++ reporting-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_09_164541) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_09_174132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,7 +55,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_09_164541) do
   create_table "activity_report_application_forms", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "reporting_period"
     t.uuid "user_id"
     t.integer "status"
     t.datetime "submitted_at"
