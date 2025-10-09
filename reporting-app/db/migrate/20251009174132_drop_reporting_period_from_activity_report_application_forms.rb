6cfba463743d291c1896eba4bde8c5027336f237
Mike Giver <mikegiver@navapbc.com>
2025-10-09 13:56:44 -0400

    linting

--- reporting-app/db/migrate/20251009174132_drop_reporting_period_from_activity_report_application_forms.rb
+++ reporting-app/db/migrate/20251009174132_drop_reporting_period_from_activity_report_application_forms.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DropReportingPeriodFromActivityReportApplicationForms < ActiveRecord::Migration[7.2]
   def change
     remove_column :activity_report_application_forms, :reporting_period, :date
