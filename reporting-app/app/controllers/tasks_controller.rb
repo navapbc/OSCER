7a81a82d734b4a1d02051a6945e3020d51de3ce0
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-10-09 12:33:51 -0500

    Add tests for filtering by status

--- reporting-app/app/controllers/tasks_controller.rb
+++ reporting-app/app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class TasksController < Strata::TasksController
+  protected
+
   def filter_tasks_by_status(tasks, status)
     status == "completed" \
         ? tasks.without_status(:pending) \
