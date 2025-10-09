c8f13503bedb205fd4bbc67707e714f7ffd2ae39
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-10-09 12:22:20 -0500

    Filter tasks by proper status

--- reporting-app/app/controllers/tasks_controller.rb
+++ reporting-app/app/controllers/tasks_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class TasksController < Strata::TasksController
+  def filter_tasks_by_status(tasks, status)
+    status == "completed" \
+        ? tasks.without_status(:pending) \
+        : tasks.with_status(:pending)
+  end
 end
