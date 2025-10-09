4a75ec63f97a6fcca10711159dc1b0405fe0e3a3
Loren Yu <loren@navapbc.com>
2025-10-08 17:26:25 -0700

    Add more cert details

--- reporting-app/app/models/certification.rb
+++ reporting-app/app/models/certification.rb
@@ -18,6 +18,19 @@ class Certification < ApplicationRecord
 
   scope :by_member_id, ->(member_id) { where(member_id:) }
 
+  def due_date
+    Date.parse(self.certification_requirements&.dig("due_date"))
+  end
+
+  def lookback_period
+    start_month = Date.parse(self.certification_requirements&.dig("months_that_can_be_certified").first)
+    end_month = Date.parse(self.certification_requirements&.dig("months_that_can_be_certified").last)
+    Strata::DateRange.new(
+      start: start_month,
+      end: end_month
+    )
+  end
+
   def member_account_email
     return unless self.member_data
 
