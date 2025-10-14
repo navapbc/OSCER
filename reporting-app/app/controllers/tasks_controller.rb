2476caa2cdc54ab1b0607a93b8fc9eb0f5d915ac
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-10-14 13:19:07 -0500

    Redirect to task show after assignment

--- reporting-app/app/controllers/tasks_controller.rb
+++ reporting-app/app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class TasksController < Strata::TasksController
-  before_action :set_task, only: [ :assign ]
+  before_action -> { set_task }, only: [ :assign ]
   
   def assign
     @task.assign(current_user.id)
+    flash["task-message"] = "Task assigned to you."
+    redirect_to @task
   end
   
   protected
@@ -14,8 +16,6 @@ class TasksController < Strata::TasksController
         ? tasks.without_status(:pending) \
         : tasks.with_status(:pending)
   end
-
-  private
 
   def set_application_form
     @application_form = ActivityReportApplicationForm.find_by(certification_case_id: @case.id) ||
