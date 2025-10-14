e662a95971c4e44018960d97b9cc4da125a7dcba
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-10-14 15:35:10 -0500

    Make tasks default due on 1 week from today

--- reporting-app/app/models/review_activity_report_task.rb
+++ reporting-app/app/models/review_activity_report_task.rb
@@ -3,4 +3,5 @@
 class ReviewActivityReportTask < Strata::Task
   # Extend the status enum with new values
   enum :status, { pending: 0, completed: 1, approved: 2, denied: 3 }
+  attribute :due_on, :date, default: -> { 7.days.from_now.to_date }
 end
--- reporting-app/app/models/review_exemption_claim_task.rb
+++ reporting-app/app/models/review_exemption_claim_task.rb
@@ -2,4 +2,5 @@
 
 class ReviewExemptionClaimTask < Strata::Task
   enum status: { pending: 0, completed: 1, approved: 2, denied: 3 }
+  attribute :due_on, :date, default: -> { 7.days.from_now.to_date }
 end
