585d92d24a06fd963934d54b407e3d14d987efb0
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-10-14 14:38:55 -0500

    Minor cleanup

--- reporting-app/app/controllers/tasks_controller.rb
+++ reporting-app/app/controllers/tasks_controller.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class TasksController < Strata::TasksController
-  before_action -> { set_task }, only: [ :assign ]
-  
   def assign
+    set_task
     @task.assign(current_user.id)
     flash["task-message"] = "Task assigned to you."
     redirect_to task_path(@task)
