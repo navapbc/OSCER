7249ef1595c50051d2d14e012af1072c857a1828
Tanner Doshier <git@doshitan.com>
2025-10-15 17:21:16 -0400

    Dynamic `months_that_can_be_certified` on factory

--- reporting-app/spec/factories/certifications/certification_requirements_factory.rb
+++ reporting-app/spec/factories/certifications/certification_requirements_factory.rb
@@ -3,8 +3,13 @@
 FactoryBot.define do
   factory :certification_certification_requirements, class: Certifications::Requirements do
     certification_date { Faker::Date.forward(days: 30) }
-    months_that_can_be_certified { [ "2025-09-01" ] }
     number_of_months_to_certify { Faker::Number.within(range: 1..3) }
+    months_that_can_be_certified do
+      Faker::Number.between(
+        from: number_of_months_to_certify,
+        to: number_of_months_to_certify + 3
+      ).times.map { |i| certification_date.beginning_of_month << i }
+    end
     due_date { Faker::Date.between(from: 30.days.from_now, to: 60.days.from_now) }
 
     # TODO: trait for certification type
