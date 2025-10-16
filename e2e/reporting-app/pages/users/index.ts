a077f4b7b179ad709f6e6a91ae6ab55c39c7d5eb
Loren Yu <loren@navapbc.com>
2025-10-15 17:42:34 -0700

    Fix casing

--- e2e/reporting-app/pages/users/index.ts
+++ e2e/reporting-app/pages/users/index.ts
@@ -1,4 +1,4 @@
 export { RegistrationPage } from './RegistrationPage';
 export { VerifyAccountPage } from './VerifyAccountPage';
 export { SignInPage } from './SignInPage';
-export { MFAPreferencePage } from './mfa/MFAPreferencePage';
+export { MfaPreferencePage } from './mfa/MfaPreferencePage';
