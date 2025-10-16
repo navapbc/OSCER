7bebe0e6a33a9c1c8b544aca2079923f6e30cd51
Loren Yu <yuloren@gmail.com>
2025-10-16 09:24:46 -0700

    Add business process preview (#95)
    - Added missing application_task("report_activities") start task
    - Removed system processes that were triggered directly as a result of
    task actions and instead trigger them in the task action itself

--- reporting-app/spec/requests/review_activity_report_tasks_spec.rb
+++ reporting-app/spec/requests/review_activity_report_tasks_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "/review_activity_report_tasks", type: :request do
   include Warden::Test::Helpers
 
   let(:user) { User.create!(email: "test@example.com", uid: SecureRandom.uuid, provider: "login.gov") }
-  let(:kase) { create(:certification_case) }
+  let(:kase) { create(:certification_case, business_process_current_step: "review_activity_report") }
   let(:task) { create(:review_activity_report_task, case: kase) }
 
   before do
@@ -21,10 +21,18 @@ RSpec.describe "/review_activity_report_tasks", type: :request do
     context "with approve action" do
       before { patch review_activity_report_task_url(task), params: { commit: I18n.t("tasks.details.approve_button") } }
 
-      it "marks task as approved" do
+      it "marks task as completed" do
         task.reload
 
-        expect(task).to be_approved
+        expect(task).to be_completed
+      end
+
+      it "marks case as approved" do
+        kase.reload
+
+        expect(kase.activity_report_approval_status).to eq("approved")
+        expect(kase.business_process_instance.current_step).to eq("end")
+        expect(kase).to be_closed
       end
 
       it "redirects back to the task" do
@@ -35,10 +43,18 @@ RSpec.describe "/review_activity_report_tasks", type: :request do
     context "with deny action" do
       before { patch review_activity_report_task_url(task), params: { commit: I18n.t("tasks.details.deny_button") } }
 
-      it "marks task as denied" do
+      it "marks task as completed" do
         task.reload
 
-        expect(task).to be_denied
+        expect(task).to be_completed
+      end
+
+      it "marks case as denied" do
+        kase.reload
+
+        expect(kase.activity_report_approval_status).to eq("denied")
+        expect(kase.business_process_instance.current_step).to eq("end")
+        expect(kase).to be_closed
       end
 
       it "redirects back to the task" do
