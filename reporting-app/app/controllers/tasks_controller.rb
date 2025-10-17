836976e6dd40ea2c6628ef728c57232142770931
Bao Nguyen <baonguyen@navapbc.com>
2025-10-17 10:54:17 -0600

    Update to set application form based on task type

--- reporting-app/app/controllers/tasks_controller.rb
+++ reporting-app/app/controllers/tasks_controller.rb
@@ -17,7 +17,15 @@ class TasksController < Strata::TasksController
   end
 
   def set_application_form
-    @application_form = ActivityReportApplicationForm.find_by(certification_case_id: @case.id) ||
-                        ExemptionApplicationForm.find_by(certification_case_id: @case.id)
+    # We do not want to grab the application form class from Certification Case
+    # because it can be tied to multiple application form types
+    @application_form = case @task.type
+    when ReviewActivityReportTask.name
+      ActivityReportApplicationForm.find_by(certification_case_id: @task.case_id)
+    when ReviewExemptionClaimTask.name
+      ExemptionApplicationForm.find_by(certification_case_id: @task.case_id)
+    else
+      raise StandardError, "Unknown task type #{@task.type}"
+    end
   end
 end
