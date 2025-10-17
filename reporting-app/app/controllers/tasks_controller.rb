b16b54c14b6d05ea6ad0649434de55cc54d084e5
Bao Nguyen <baonguyen@navapbc.com>
2025-10-17 11:32:34 -0600

    Revise to not use case statement

--- reporting-app/app/controllers/tasks_controller.rb
+++ reporting-app/app/controllers/tasks_controller.rb
@@ -19,13 +19,6 @@ class TasksController < Strata::TasksController
   def set_application_form
     # We do not want to grab the application form class from Certification Case
     # because it can be tied to multiple application form types
-    @application_form = case @task.type
-    when ReviewActivityReportTask.name
-      ActivityReportApplicationForm.find_by(certification_case_id: @task.case_id)
-    when ReviewExemptionClaimTask.name
-      ExemptionApplicationForm.find_by(certification_case_id: @task.case_id)
-    else
-      raise StandardError, "Unknown task type #{@task.type}"
-    end
+    @application_form = @task.class.application_form_class.find_by(certification_case_id: @task.case_id)
   end
 end
--- reporting-app/app/models/review_exemption_claim_task.rb
+++ reporting-app/app/models/review_exemption_claim_task.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ReviewExemptionClaimTask < Strata::Task
+  def self.application_form_class
+    ExemptionApplicationForm
+  end
 end
